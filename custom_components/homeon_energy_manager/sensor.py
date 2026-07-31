37de3aa0718fda0d0a1e8df5c8048e044c37386a
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 10:24:15 +0200

    Update manager sensor device version to 1.0.3

--- custom_components/homeon_energy_manager/sensor.py
+++ custom_components/homeon_energy_manager/sensor.py
@@ -190,7 +190,7 @@ class HomeOnSensor(CoordinatorEntity, SensorEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.0.2",
+            "sw_version": "1.0.3",
         }
 
     @property
