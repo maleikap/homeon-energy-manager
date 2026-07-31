e3b78347220c3fc47f1a5e9d0d6a21700c5d925c
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 09:58:58 +0200

    Publish HomeOn Energy Manager 1.0.0 device version

--- custom_components/homeon_energy_manager/switch.py
+++ custom_components/homeon_energy_manager/switch.py
@@ -44,7 +44,7 @@ class HomeOnSwitch(CoordinatorEntity, SwitchEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "0.2.43",
+            "sw_version": "1.0.0",
         }
 
     @property
