10758e4b3ea5ed99a816a1ed33d7196baeff2d96
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 10:09:14 +0200

    Update manager switch device version to 1.0.1

--- custom_components/homeon_energy_manager/switch.py
+++ custom_components/homeon_energy_manager/switch.py
@@ -44,7 +44,7 @@ class HomeOnSwitch(CoordinatorEntity, SwitchEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.0.0",
+            "sw_version": "1.0.1",
         }
 
     @property
