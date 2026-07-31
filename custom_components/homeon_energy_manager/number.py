565d85cd9353a49f36e61544127dcab1c54a31fd
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 10:53:35 +0200

    Update manager number device version to 1.0.6

--- custom_components/homeon_energy_manager/number.py
+++ custom_components/homeon_energy_manager/number.py
@@ -143,7 +143,7 @@ class HomeOnNumber(CoordinatorEntity, NumberEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.0.5",
+            "sw_version": "1.0.6",
         }
 
     @property
