bc440a74eefb79bc952ab8df6f7284ceb3e58306
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 15:09:06 +0200

    Close expired PV windows and sell at best price

--- custom_components/homeon_energy_manager/number.py
+++ custom_components/homeon_energy_manager/number.py
@@ -143,7 +143,7 @@ class HomeOnNumber(CoordinatorEntity, NumberEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.1.0",
+            "sw_version": "1.1.2",
         }
 
     @property
