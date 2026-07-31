e1e756efa4cd8ccdab5486b8d71fbb40f331af1e
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 19:45:38 +0200

    Add safe 24h energy optimizer

--- custom_components/homeon_energy_manager/switch.py
+++ custom_components/homeon_energy_manager/switch.py
@@ -44,7 +44,7 @@ class HomeOnSwitch(CoordinatorEntity, SwitchEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.1.6",
+            "sw_version": "1.2.0",
         }
 
     @property
