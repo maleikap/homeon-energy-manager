70e5a208e988f1739e44e9ea79da426154c19d24
HomeON <98586540+maleikap@users.noreply.github.com>
2026-07-31 18:32:40 +0200

    Prepare v1.1.4 better-price waiting

--- custom_components/homeon_energy_manager/number.py
+++ custom_components/homeon_energy_manager/number.py
@@ -143,7 +143,7 @@ class HomeOnNumber(CoordinatorEntity, NumberEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.1.3",
+            "sw_version": "1.1.4",
         }
 
     @property
