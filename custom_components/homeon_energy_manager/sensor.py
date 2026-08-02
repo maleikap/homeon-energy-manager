514a0e1bbc13f7e6d65396222f3ef6966e4084d8
HomeON <98586540+maleikap@users.noreply.github.com>
2026-08-02 14:31:23 +0200

    Prepare HomeOn Energy Manager v1.2.1

--- custom_components/homeon_energy_manager/sensor.py
+++ custom_components/homeon_energy_manager/sensor.py
@@ -221,7 +221,7 @@ class HomeOnSensor(CoordinatorEntity, SensorEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.2.0",
+            "sw_version": "1.2.1",
         }
 
     @property
