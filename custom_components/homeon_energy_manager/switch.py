591d918327fb97e89a2b53f33b619bc1846bc930
HomeON <98586540+maleikap@users.noreply.github.com>
2026-08-04 20:07:21 +0200

    Bump HomeOn Energy Manager to 1.2.7

--- custom_components/homeon_energy_manager/switch.py
+++ custom_components/homeon_energy_manager/switch.py
@@ -44,7 +44,7 @@ class HomeOnSwitch(CoordinatorEntity, SwitchEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.2.6",
+            "sw_version": "1.2.7",
         }
 
     @property
