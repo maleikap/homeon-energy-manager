73eb36ec9035cfcc3f8aa0a7ae0a812131574a3f
HomeON <98586540+maleikap@users.noreply.github.com>
2026-09-16 18:48:28 +0200

    Zasilaj dom z pełnej baterii bez produkcji PV

--- custom_components/homeon_energy_manager/number.py
+++ custom_components/homeon_energy_manager/number.py
@@ -148,7 +148,7 @@ class HomeOnNumber(CoordinatorEntity, NumberEntity):
             "name": "HomeOn Energy Manager",
             "manufacturer": "HomeOn",
             "model": "Energy Manager",
-            "sw_version": "1.2.18",
+            "sw_version": "1.2.19",
         }
 
     @property
